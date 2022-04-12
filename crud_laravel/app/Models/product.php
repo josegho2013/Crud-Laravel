59adfc49bc56f4003f9b2409ed7ad0155b59818d
jose hernandez <josegho2013@gmail.com>
2022-04-12 09:41:39 -0300

    add controller

--- crud_laravel/app/Models/product.php
+++ crud_laravel/app/Models/product.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class product extends Model
+/**
+ * Class Product
+ *
+ * @property $id
+ * @property $description
+ * @property $price
+ * @property $stock
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Product extends Model
 {
-    use HasFactory;
+    
+    static $rules = [
+		'description' => 'required',
+		'price' => 'required',
+		'stock' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['description','price','stock'];
+
+
+
 }
